9cf40d552d83308ecb2cc859b02c662c1468694e
tate-aabb <tatevik89@yahoo.com>
2023-03-02 19:21:17 +0400

    Changes in settings.py, urls.py, added app's urls.py, base, static files, homepage.html.
    New "ForbesList" model added, in views.py forbes list scraper added.

--- sponsorhub/models.py
+++ sponsorhub/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class ForbesList(models.Model):
+    rank = models.IntegerField()
+    name = models.CharField(max_length=200)
+    net_worth = models.CharField(max_length=200)
+    age = models.IntegerField()
--- sponsorhub/views.py
+++ sponsorhub/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from .models import ForbesList
 
 
 def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def scrapeandsave(request):
+    url = "https://www.forbes.com/forbes-400/"
+    result = requests.get(url)
+    soup = BeautifulSoup(result.content, 'html.parser')
+
+    #Scraping the data
+    for person in soup.find_all('div', {'class': 'Table_tableRow__M82uU'})[:16]:
+
+        rank = person.find('div', {'class': 'Table_rank__KnjUY'}).text
+        name = person.find('div', {'class': 'Table_personName__UO41W'}).text
+        net_worth = person.find('div', {'class': 'Table_netWorth__py4xM'}).text
+        age = person.find('div', {'class': 'Table_age__CHhfQ'}).text
+
+        #Saving the data in DB
+        forbes_list = ForbesList(rank=rank, name=name, net_worth=net_worth, age=age)
+        forbes_list.save()
+
     return render(request, 'homepage.html')
