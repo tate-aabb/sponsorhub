7866843d8318df9e842d124641cbfac83d7f1fae
tate-aabb <tatevik89@yahoo.com>
2023-03-03 20:12:26 +0400

    Changes in settings.py, urls.py, added app's urls.py, base, static files, homepage.html.
    New "ForbesList" model added, in views.py forbes list scraper added.
    
    Design changes in homepage.html, about block is added
    
    Changes in Homepage design, base.html
    
    forbes_list added to the admin.py, views.py homepage function and to the homepage.html.
    
    scraper fixed in urls.py, views.py

--- sponsorhub/views.py
+++ sponsorhub/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
 import requests
 from bs4 import BeautifulSoup
 from .models import ForbesList
@@ -17,13 +18,11 @@ def scrapeandsave(request):
     #Scraping the data
     for person in soup.find_all('div', {'class': 'Table_tableRow__M82uU'})[:16]:
 
-        rank = person.find('div', {'class': 'Table_rank__KnjUY'}).text
+        rank = person.find('div', {'class': 'Table_rank__KnjUY'}).text.replace('.', '')
         name = person.find('div', {'class': 'Table_personName__UO41W'}).text
         net_worth = person.find('div', {'class': 'Table_netWorth__py4xM'}).text
         age = person.find('div', {'class': 'Table_age__CHhfQ'}).text
 
-        #Saving the data in DB
-        forbes_list = ForbesList(rank=rank, name=name, net_worth=net_worth, age=age)
-        forbes_list.save()
+        ForbesList.objects.get_or_create(rank=rank, name=name, net_worth=net_worth, age=age)
 
-    return render(request, 'homepage.html')
+    return HttpResponse('Data successfully scraped')
