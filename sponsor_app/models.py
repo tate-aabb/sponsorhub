b9d43f7e9aacc2e6fda2cd06b522a835fa27d60f
Arsen5457 <xachatryan2002@inbox.ru>
2023-03-20 18:00:33 +0300

    commit

--- sponsor_app/models.py
+++ sponsor_app/models.py
@@ -1,13 +1,9 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
-
-
 
 
 class Sponsorpost(models.Model):
     business_name = models.CharField(max_length=50)
-    about_sponsor = models.TextField()
     business_industry = models.CharField(max_length=100)
-    budget = models.IntegerField(validators=[MinValueValidator(1000), MaxValueValidator(500000)], default=5000)
+    about_sponsor = models.TextField()
+    budget = models.IntegerField(default=5000)
     email = models.EmailField()
-
