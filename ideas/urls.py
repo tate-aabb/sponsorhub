ea7884de6ab6b26435c553e74ae9b1c27996c50d
Tatev <123472016+tate-aabb@users.noreply.github.com>
2023-03-23 16:46:45 +0400

    Changes in settings.py, urls.py, added app's urls.py, base, static files, homepage.html.
    New "ForbesList" model added, in views.py forbes list scraper added.
    
    Design changes in homepage.html, about block is added
    
    Changes in Homepage design, base.html
    
    forbes_list added to the admin.py, views.py homepage function and to the homepage.html.
    
    scraper fixed in urls.py, views.py
    
    ForbesList table is added and styled.
    
    ideas app added
    
    contact page added, with model, forms, view and html files.
    
    footer in base.html is modified
    
    homepage link in base.html is changed
    
    ideas app completely modified, CRUD added, search added

--- ideas/urls.py
+++ ideas/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.IdeasBlog, name="ideas"),
-    path("new_ideas", views.New_Ideas.as_view(), name="new")
+    path("", views.IdeasBlog.as_view(), name="ideas"),
+    path('', views.search, name='search'),
+    path('idea/<int:pk>/', views.IdeasDetail.as_view(), name="ideas_detail"),
+    path("new_ideas", views.NewIdeas.as_view(), name="new"),
+    path('idea/<int:pk>/update/', views.IdeasUpdate.as_view(), name="update"),
+    path('idea/<int:pk>/delete/', views.IdeasDelete.as_view(), name="delete"),
 ]
--- ideas/views.py
+++ ideas/views.py
@@ -1,29 +1,66 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, DetailView
 from .models import IdeasModel
 from django.shortcuts import render
-from django.views.generic.edit import CreateView, FormView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .forms import Add_Ideas
-from sponsor_app.models import Sponsorpost
-from sponsor_app.forms import SponsorForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 from accounts.models import Type, CustomUser
 
 
+# @user_passes_test(lambda user: user.custom_user.get().user_type == Type.CONTRIBUTOR)
 
-@login_required()
-@user_passes_test(lambda user: user.custom_user.get().user_type == Type.CONTRIBUTOR)
-def IdeasBlog(request):
-    Sponsor = Sponsorpost.objects.all()
-    return render(request, "ideas.html", {"Sponsor": Sponsor})
-
-
-
-class IdeasPage(FormView):
-    form_class = SponsorForm
-    template_name = "ideas.html"
+# @login_required
+class IdeasBlog(ListView):
+    model = IdeasModel
+    template_name = 'ideas.html'
 
 
+# @user_passes_test(lambda user: user.custom_user.get().user_type == Type.CONTRIBUTOR)
+# @login_required
+class IdeasDetail(DetailView):
+    model = IdeasModel
+    template_name = 'ideas_detail.html'
 
-class New_Ideas(CreateView):
-    form_class = Add_Ideas
-    template_name = "New_ideas.html"
+
+# @login_required
+class NewIdeas(CreateView):
+    model = IdeasModel
+    template_name = 'New_ideas.html'
+    fields = ["Title", "About_contributor", "Idea_industry", "Idea_text", "Budget", "Contact_email"]
+
+
+class IdeasUpdate(UpdateView):
+    model = IdeasModel
+    template_name = 'ideas_update.html'
+    fields = ["Title", "About_contributor", "Idea_industry", "Idea_text", "Budget", "Contact_email"]
+
+
+class IdeasDelete(DeleteView):
+    model = IdeasModel
+    template_name = 'ideas_delete.html'
+    success_url = reverse_lazy('ideas')
+
+
+def search(request):
+    searchTerm = request.GET.get('searchIdea')
+    if searchTerm:
+        ideas = IdeasModel.objects.filter(title__icontains=searchTerm)
+    else:
+        ideas = IdeasModel.objects.all()
+    return render(request, 'ideas.html',
+                  {'searchTerm': searchTerm, 'ideas': ideas})
+
+
+# def IdeasBlog(request):
+#     Sponsor = Sponsorpost.objects.all()
+#     return render(request, "ideas.html", {"Sponsor": Sponsor})
+
+
+
+# class IdeasPage(FormView):
+#     form_class = SponsorForm
+#     template_name = "ideas.html"
+
+
+#
